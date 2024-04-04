8fcc09e95bbee4a1ec5937bb635c8f2d22cb9da5
Yitbarek-Wolde <yitbarek.ayni@gmail.com>
2024-04-03 23:49:06 -0500

    renamed and reuploded, check push/commit history to verify

---
+++ WAD1-Apr-01-24.js
@@ -0,0 +1,34 @@
+// 1. Create a function using function declaration named sum with one parameter of Array type, the
+// returned result is the sum of all elements which are greater than 20.
+
+// 2. Create a function using function expression named getNewArray with one parameter of String
+// Array, return a new array which contains all string, length is greater than and equal to 5, and
+// contains letter ‘a’.
+
+// 3. Implement an arrow function with feature below:
+
+// concat('hi', [1,2,3], ['Hello','world']) -> return result: ['h', 'i', 1,2,3, 'Hello','world']
+
+function sum(arr) {
+    return arr.reduce((acc, cur) => cur > 20 ? acc + cur : acc)
+}
+
+const getNewArray = function (arr) {
+    return arr.filter(word => word.length >= 5 && word.includes('a'))
+}
+
+const concat = (...arr) => {
+    return arr.reduce((acc, cur) => {(typeof(cur) == 'object' ? acc.push(...cur) : acc.push(...cur.split('')))
+    return acc}, [])
+
+}
+
+let arrOfsum = [22, 10, 30, 5]
+let arrOfwords = ['application', 'was', 'development', 'starship']
+
+
+console.log(sum(arrOfsum))
+console.log('##########################')
+console.log(getNewArray(arrOfwords))
+console.log('##########################')
+console.log(concat('hi', [1,2,3], ['Hello','world']))
