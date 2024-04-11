bca12305de3196bbde0350631b992ca39e73e2a9
Yitbarek-Wolde <yitbarek.ayni@gmail.com>
2024-04-10 19:23:34 -0500

    data successfully fetched from my github using useEffect

--- workshop-apr-4-24/src/App.tsx
+++ workshop-apr-4-24/src/App.tsx
@@ -19,50 +19,6 @@ type datatypes = {
   like: number;
 }
 
-
-
-// Comment List data
-// const defaultList: any = [
-//   {
-//     // comment id
-//     rpid: 3,
-//     // user info
-//     user: {
-//       uid: '13258165',
-//       avatar: '',
-//       uname: 'Jay Zhou',
-//     },
-//     // comment content
-//     content: 'Nice, well done',
-//     // created datetime
-//     ctime: '10-18 08:15',
-//     like: 88,
-//   },
-//   {
-//     rpid: 2,
-//     user: {
-//       uid: '36080105',
-//       avatar: '',
-//       uname: 'Song Xu',
-//     },
-//     content: 'I search for you thousands of times, from dawn till dusk.',
-//     ctime: '11-13 11:29',
-//     like: 88,
-//   },
-//   {
-//     rpid: 1,
-//     user: {
-//       uid: '30009257',
-//       avatar,
-//       uname: 'John',
-//     },
-//     content: 'I told my computer I needed a break... now it will not stop sending me vacation ads.',
-//     ctime: '10-19 09:00',
-//     like: 66,
-//   },
-// ]
-// current logged in user info
-
 const user = {
   rpid: '',
   uid: '3030092349857',
@@ -135,10 +91,7 @@ function Reply(props: propType) {
 }
 const App = () => {
 
- 
-
   const [commentList, setCommentList] = useState<datatypes[]>(_.orderBy([], 'like', 'desc'));
-  let [count, setCount] = useState<number>(commentList.length);
   const [activeType, setActiveType] = useState('hot');
 
   const textRef = useRef<HTMLTextAreaElement | null>(null);
@@ -165,11 +118,11 @@ const App = () => {
   }
 
   const userPost = () => {
-    setCount(++count)
+   
     let newComment = {
       rpid: uuidv4(),
       user,
-      content: textRef.current?.value,
+      content: textRef.current!.value,
       ctime: dayjs(Date.now()).format('MM-DD HH:mm'),
       like: 20
     }
@@ -228,6 +181,7 @@ const App = () => {
               className="reply-box-textarea"
               placeholder="tell something..."
               ref={textRef}
+             
             />
             {/* post button */}
             <div className="reply-box-send" onClick={userPost}>
